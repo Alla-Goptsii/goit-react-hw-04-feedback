66b38ae2c9b9bc58418a59917006415d582021b2
Alla-Goptsii <99249674+Alla-Goptsii@users.noreply.github.com>
2022-12-10 21:50:32 +0200

    add useEffect App
    useEffect countTotalFeedback and countPositiveFeedbackPercentage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
 import { Statistics } from '../Statistics/Statistics';
 import { Section } from '../Section/Section';
@@ -9,6 +9,9 @@ export default function App(event) {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [countTotalFeedback, setCountTotalFeedback] = useState(0);
+  const [countPositiveFeedbackPercentage, setCountPositiveFeedbackPercentage] =
+    useState(0);
 
   const onLeaveFeedback = value => {
     switch (value) {
@@ -27,13 +30,11 @@ export default function App(event) {
     }
   };
 
-  const countTotalFeedback = () => {
-    return good + neutral + bad;
-  };
-
-  const countPositiveFeedbackPercentage = Math.round(
-    (good / countTotalFeedback()) * 100
-  );
+  useEffect(() => {
+    const allFeedback = good + neutral + bad;
+    setCountTotalFeedback(allFeedback);
+    setCountPositiveFeedbackPercentage(Math.round((good / allFeedback) * 100));
+  }, [good, neutral, bad]);
 
   return (
     <Box>
@@ -44,12 +45,12 @@ export default function App(event) {
         />
       </Section>
       <Section title={'Statistics'}>
-        {countTotalFeedback() > 0 ? (
+        {countTotalFeedback > 0 ? (
           <Statistics
             good={good}
             neutral={neutral}
             bad={bad}
-            total={countTotalFeedback()}
+            total={countTotalFeedback}
             positivePercentage={countPositiveFeedbackPercentage}
           />
         ) : (
